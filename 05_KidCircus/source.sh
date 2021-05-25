490c4bcc0f35615bf7045d98fef9280d14c63233
kmranrg <kmranrg@gmail.com>
2021-05-25 17:44:16 +0530

    conditional statements

---
+++ 05_KidCircus/source.sh
@@ -0,0 +1,12 @@
+#! /bin/bash
+
+age=15
+if [ $age -ge 13 ] && [ $age -le 19 ]
+then
+    echo "Welcome to Kids Circus. Enjoy amazing rides!"
+elif [ $age -lt 13 ]
+then
+    echo "You are too small. See you soon."
+else
+    echo "Oh O! This circus is just for kids."
+fi
