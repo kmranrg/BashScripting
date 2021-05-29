053fcaad50b04fa34549c731f094d58ca7e17531
kmranrg <kmranrg@gmail.com>
2021-05-30 00:10:54 +0530

    special commands

---
+++ 08_SpecialCommands/source1.sh
@@ -0,0 +1,37 @@
+#! /bin/bash
+
+printf "\nChecking OS details\n"
+
+neofetch
+
+: "
+OUTPUT:
+
+Checking OS details
+            .-/+oossssoo+/-.
+        `:+ssssssssssssssssss+:`
+      -+ssssssssssssssssssyyssss+-
+    .ossssssssssssssssssdMMMNysssso.
+   /ssssssssssshdmmNNmmyNMMMMhssssss/
+  +ssssssssshmydMMMMMMMNddddyssssssss+
+ /sssssssshNMMMyhhyyyyhmNMMMNhssssssss/
+.ssssssssdMMMNhsssssssssshNMMMdssssssss.
++sssshhhyNMMNyssssssssssssyNMMMysssssss+   kmranrg@kmranrg-ubuntu 
+ossyNMMMNyMMhsssssssssssssshmmmhssssssso   ---------------------- 
+ossyNMMMNyMMhsssssssssssssshmmmhssssssso   OS: Ubuntu 21.04 x86_64 
++sssshhhyNMMNyssssssssssssyNMMMysssssss+   Host: Vostro 3559 
+.ssssssssdMMMNhsssssssssshNMMMdssssssss.   Kernel: 5.11.0-17-generic 
+ /sssssssshNMMMyhhyyyyhdNMMMNhssssssss/    Uptime: 1 day, 2 hours, 9 mins 
+  +sssssssssdmydMMMMMMMMddddyssssssss+     Packages: 2132 (dpkg), 12 (snap) 
+   /ssssssssssshdmNNNNmyNMMMMhssssss/      Shell: bash 5.1.4 
+    .ossssssssssssssssssdMMMNysssso.       Resolution: 1366x768 
+      -+sssssssssssssssssyyyssss+-         DE: Unity 
+        `:+ssssssssssssssssss+:`           WM: Mutter 
+            .-/+oossssoo+/-.               WM Theme: Adwaita 
+                                           Theme: WhiteSur-dark-solid [GTK2/3] 
+                                           Icons: WhiteSur-dark [GTK2/3] 
+                                           Terminal: vscode 
+                                           CPU: Intel i5-6200U (4) @ 2.800GHz 
+                                           GPU: Intel Skylake GT2 [HD Graphics 520] 
+                                           Memory: 3092MiB / 7844MiB 
+"
